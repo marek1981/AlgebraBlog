0453a18487e1773247f8219736e64bbbe32422df
programeri <programeri@ucione.local>
2017-05-03 20:47:13 +0200

    Dodana datoteka Items controller

--- app/Http/Controllers/Itemscontroller.php
+++ app/Http/Controllers/Itemscontroller.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use DB;
 
 class Itemscontroller extends Controller
 {
@@ -13,7 +14,8 @@ class Itemscontroller extends Controller
      */
     public function index()
     {
-        //
+		$items = DB::table('items')->get();
+        return view ('items.index', compact('items'));
     }
 
     /**
@@ -45,7 +47,8 @@ class Itemscontroller extends Controller
      */
     public function show($id)
     {
-        //
+		$item = DB::table('items')->find($id);
+        return view('items.show', compact('$item'));
     }
 
     /**

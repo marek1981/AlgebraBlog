91c5a70bfcf66229b8ad1faf13d33adcf15fbe5f
programeri <programeri@ucione.local>
2017-05-05 20:31:16 +0200

    dodan link za brisanje

--- resources/views/items/show.blade.php
+++ resources/views/items/show.blade.php
@@ -6,6 +6,13 @@
         <div class="col-md-8 col-md-offset-2">
 		<h2>{{ $item->title }}</h2>
 		<div>{{ $item->content }}</div>
+		<div>{{ $item->created_at}}</div>
+		<!--Provjera da li je user vlasnik itema-->
+		@if ( auth()->id() == $item->user_id)
+			
+	   <br><a href="{{url('item/'.$item->id.'/del')}}" class="btn btn-danger">Obriši</a>
+	   
+	   @endif
          </div>
     </div>
 </div>
